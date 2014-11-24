8521b4327df8d529ceb37f48e6be999fec8a142b
srt32 <simon.taranto@gmail.com>
2014-11-24 11:39:49 -0500

    Stubbed out endpoint

---
+++ README.md
@@ -0,0 +1,7 @@
+WIP
+
+golang service to parse gumroad webhooks and process github team memberships
+
+run `gin` to auto reload the code
+
+send a request to `localhost:3000/purchase`
--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/codegangsta/negroni"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	loadEnv()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/purchase", newPurchase)
 
@@ -18,11 +23,44 @@ func main() {
 }
 
 func newPurchase(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "From the handler!")
+	data := json.NewDecoder(req.Body)
+	var purchase Purchase
 
-	// parse the params
+	if err := data.Decode(&purchase); err != nil {
+		http.Error(
+			w,
+			fmt.Sprintf("Unable to decode json: %s", err),
+			http.StatusBadRequest,
+		)
+		return
+	}
 
-	// get the github username and product
+	UserName := purchase.UserName
+	productId := purchase.ProductId
+	teamId := productId // requires mapping
 
+	err := addUserToTeam(UserName, teamId)
+
+	if err != nil {
+		log.Fatal("Error adding user to team", err)
+	}
+}
+
+type Purchase struct {
+	UserName  string `json:"email"`
+	ProductId int    `json:"full_name"`
+}
+
+func addUserToTeam(UserName string, teamId int) error {
 	// add the user to the GH team
+	fmt.Print("adding user")
+
+	return nil
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
